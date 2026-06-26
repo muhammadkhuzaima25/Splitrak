da92bc03e04b74c3805a27ff07a793b4a49d2c5e
Muhammad Khuzaima <muhammadkhuzaima.gcu@gmail.com>
2026-06-26 18:37:56 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,8 +12,19 @@ app.use(cors({
     origin: ['http://localhost:5173', 'https://splitrak.vercel.app'], 
     credentials: true 
 }));
-
 app.use(express.json());
+
+let dbReady = false;
+const ensureDB = async () => {
+    if (!dbReady) {
+        dbReady = await connectDB();
+    }
+};
+
+app.use(async (req, res, next) => {
+    await ensureDB();
+    next();
+});
 
 app.get('/api/health', async (req, res) => {
     const dbConnected = getDBStatus();
@@ -28,18 +39,6 @@ app.get('/api/health', async (req, res) => {
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/vendors', require('./routes/vendorRoutes'));
 app.use('/api', require('./routes/quotationRoutes'));
-
-let dbReady = false;
-const ensureDB = async () => {
-    if (!dbReady) {
-        dbReady = await connectDB();
-    }
-};
-
-app.use(async (req, res, next) => {
-    await ensureDB();
-    next();
-});
 
 if (!process.env.VERCEL) {
     const PORT = process.env.PORT || 5000;
